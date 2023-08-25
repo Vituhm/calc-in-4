52912f1d329dc64efa564ee6a15cf4f73bf550b0
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 19:55:10 -0300

    versão 1.3 - soma, sub, multi & div

--- calculadora.py
+++ calculadora.py
@@ -14,3 +14,10 @@ elif op == 2:
     print("Resultado: ", n1, "-", n2, "=", n1 - n2)
 elif op == 3:
     print("Resultado: ", n1, "*", n2, "=", n1 * n2)
+elif op == 4:
+    if n2 == 0:
+        print("Não é possível dividir por Zero.")
+    else:
+        print("Resultado: ", n1, "/", n2, "=", n1 / n2)
+else:
+    print("Opção inválida")
