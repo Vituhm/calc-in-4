dc1cdba530ecfa44f9dc9220e2e3a18e5e07de73
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 19:47:30 -0300

    versão 1.1 - soma e sub

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,13 @@
 n1=(int(input("Informe um número: ")))
 n2=(int(input("Informe um número: ")))
-print("Resultado: ", n1, "+", n2, "=", n1 + n2)
+
+print("Escolha a operação a ser realizada: ")
+print("1 = Adição (+)")
+print("2 = Subtração (-)")
+
+op=(int(input("Opção: ")))
+
+if op == 1:
+    print("Resultado: ", n1, "+", n2, "=", n1 + n2)
+if op == 2:
+    print("Resultado: ", n1, "-", n2, "=", n1 - n2)
