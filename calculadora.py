f502d806e9abad0ede4cd43eaa7972229aa77bd4
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 19:52:41 -0300

    versão 1.2 - soma, sub & multi

--- calculadora.py
+++ calculadora.py
@@ -4,10 +4,13 @@ n2=(int(input("Informe um número: ")))
 print("Escolha a operação a ser realizada: ")
 print("1 = Adição (+)")
 print("2 = Subtração (-)")
+print("3 = Multiplicação (*)")
 
 op=(int(input("Opção: ")))
 
 if op == 1:
     print("Resultado: ", n1, "+", n2, "=", n1 + n2)
-if op == 2:
+elif op == 2:
     print("Resultado: ", n1, "-", n2, "=", n1 - n2)
+elif op == 3:
+    print("Resultado: ", n1, "*", n2, "=", n1 * n2)
